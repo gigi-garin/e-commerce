57ee92aa85702af0355d5069190352849b4d0709
J Gigi Garin <gigigarin2003@gmail.com>
2024-10-30 21:27:42 +0530

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,28 +5,23 @@ import Home from './components/Home';
 import About from './components/About';
 import Product from './components/Product';
 import Contact from './components/Contact';
-import { Route, Routes, Navigate } from 'react-router-dom';
 import ProductDetail from './components/ProductDetail';
 import Cart from './components/Cart';
 import Checkout from './components/Checkout';
 
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
 function App() {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Product />} />
-        <Route path="/products/:id" element={<ProductDetail />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<Navigate to="/" />} /> {/* Redirects unknown paths to Home */}
-      </Routes>
-      <Footer />
-    </>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                {/* Example Redirect */}
+                <Redirect from="/redirect" to="/" />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
